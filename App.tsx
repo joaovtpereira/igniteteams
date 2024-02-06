c6e7a2b3b1f16c0be441ada491b632af1bf6b4c5
João Vitor Pereira <joaovitorpereira@MacBook-Pro-de-Joao.local>
2024-02-05 22:46:15 -0300

    initial commit

---
+++ App.tsx
@@ -0,0 +1,20 @@
+import { ActivityIndicator } from "react-native";
+import { Groups } from "@screens/Groups";
+import theme from "./src/theme/index";
+import { ThemeProvider } from "styled-components";
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from "@expo-google-fonts/roboto";
+import { Loading } from "@components/Loading";
+
+export default function App() {
+  const [fontsLoad] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+
+  return (
+    <ThemeProvider theme={theme}>
+      {fontsLoad ? <Groups /> : <Loading />}{" "}
+    </ThemeProvider>
+  );
+}
