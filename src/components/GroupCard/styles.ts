56d1b5ce0d90364c32bc2b78926674e590686d2b
joaovtpereira <joaovitorpessoal2@gmail.com>
2024-02-05 23:21:15 -0300

    add component GroupCard

---
+++ src/components/GroupCard/styles.ts
@@ -0,0 +1,32 @@
+import styled from "styled-components/native";
+import { CaretLeft } from 'phosphor-react-native'
+import { TouchableOpacity } from "react-native";
+import { UsersThree } from "phosphor-react-native"
+
+export const Container = styled(TouchableOpacity)`
+    align-items: center;
+    width: 100%;
+    flex-direction: row;
+    height: 90px;
+    background-color: ${({theme}) => theme.COLORS.GRAY_500};
+    border-radius: 6px;
+
+    padding: 24px;
+    margin-bottom: 12px;
+`;
+
+export const Title = styled.Text`
+    font-size: ${({theme}) => theme.FONT_SIZE.MD}px;
+    font-family: ${({theme}) => theme.FONT_FAMILY.REGULAR};
+    color: ${({theme}) => theme.COLORS.GRAY_200};
+`;
+
+export const Icon = styled(UsersThree).attrs(({theme}) => ({
+    size: 32,
+    color: theme.COLORS.GREEN_700,
+    weight: "fill"
+}))`
+    margin-right: 20px;
+`
+
+
--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,12 +1,14 @@
 import { Header } from "@components/Header";
 import { Container } from "./styles";
 import { HighLight } from "@components/HighLight";
+import { GroupCard } from "@components/GroupCard";
 
 export function Groups() {
   return (
     <Container>
       <Header />
       <HighLight title="Turmas" subtitle="Jogue com a sua turma" />
+      <GroupCard title="Lorem Ipsum Nova York" />
     </Container>
   );
 }
