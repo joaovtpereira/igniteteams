db2abf21f91c12307f6778e3bde66764efef5874
joaovtpereira <joaovitorpessoal2@gmail.com>
2024-04-03 22:55:21 -0300

    add Button component

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -5,6 +5,7 @@ import { HighLight } from "@components/HighLight";
 import { GroupCard } from "@components/GroupCard";
 import { FlatList } from "react-native";
 import { ListEmpty } from "@components/ListEmpty";
+import { Button } from "@components/Button";
 
 export function Groups() {
   const [groups, setGroups] = useState<string[]>([]);
@@ -21,6 +22,8 @@ export function Groups() {
         contentContainerStyle={groups.length === 0 ? { flex: 1 } : undefined}
         ListEmptyComponent={() => <ListEmpty message="Lista vazia" />}
       />
+
+      <Button title="Criar nova turma" />
     </Container>
   );
 }
