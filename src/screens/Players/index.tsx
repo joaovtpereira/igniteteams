c5a6d43de08976f758017b638f7dcf6aaf11c6f3
joaovtpereira <joaovitorpessoal2@gmail.com>
2024-04-10 21:43:44 -0300

    add: ButtonIcon, Filter and Players Components

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -1,8 +1,16 @@
 import { Header } from "@components/Header";
-import { Container, Content } from "./styles";
+import { Container, Form, HeaderList, NumberOfPlayers } from "./styles";
 import { HighLight } from "@components/HighLight";
+import { ButtonIcon } from "@components/ButtonIcon";
+import { Input } from "@components/Input";
+import { Filter } from "@components/Filter";
+import { FlatList } from "react-native";
+import { useState } from "react";
 
 export function Players() {
+  const [team, setTeam] = useState("");
+  const [players, setPLayers] = useState([]);
+
   return (
     <Container>
       <Header showBackButton />
@@ -12,7 +20,26 @@ export function Players() {
         subtitle="Adicione a galera e separe o time"
       />
 
-      <Content></Content>
+      <Form>
+        <Input placeholder="Nome da pessoa" autoCorrect={false} />
+        <ButtonIcon icon="add" />
+      </Form>
+
+      <HeaderList>
+        <FlatList
+          data={["Time A"]}
+          keyExtractor={(item) => item}
+          renderItem={({ item }) => (
+            <Filter
+              title={item}
+              isActive={team === item}
+              onPress={() => setTeam(item)}
+            />
+          )}
+        />
+
+        <NumberOfPlayers>{players.length}</NumberOfPlayers>
+      </HeaderList>
     </Container>
   );
 }
