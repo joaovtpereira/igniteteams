a201ac12676d9ef57364b59aa3cabd1761940d0b
João Vitor Pereira <joaovitorpessoal2@gmail.com>
2024-08-08 22:36:43 -0300

    definindo tipagem para rotas

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -6,9 +6,15 @@ import { GroupCard } from "@components/GroupCard";
 import { FlatList } from "react-native";
 import { ListEmpty } from "@components/ListEmpty";
 import { Button } from "@components/Button";
+import { useNavigation } from "@react-navigation/native";
 
 export function Groups() {
   const [groups, setGroups] = useState<string[]>([]);
+  const navigation = useNavigation();
+
+  function handleNewGroup() {
+    navigation.navigate("new");
+  }
 
   return (
     <Container>
@@ -23,7 +29,7 @@ export function Groups() {
         ListEmptyComponent={() => <ListEmpty message="Lista vazia" />}
       />
 
-      <Button title="Criar nova turma" />
+      <Button title="Criar nova turma" onPress={handleNewGroup} />
     </Container>
   );
 }
