4bb9664c4c554bf738f57967acd7dd480e78206a
João Vitor Pereira <joaovitorpessoal2@gmail.com>
2024-08-08 22:47:18 -0300

    add handleGoBack

--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -3,8 +3,16 @@ import { Container, Content, Icon } from "./styles";
 import { HighLight } from "@components/HighLight";
 import { Button } from "@components/Button";
 import { Input } from "@components/Input";
+import { useNavigation } from "@react-navigation/native";
 
 export function NewGroup() {
+  const navigation = useNavigation();
+
+  function handleNewGroup() {
+    navigation.navigate("players", {
+      group: "Rocket",
+    });
+  }
   return (
     <Container>
       <Header showBackButton />
@@ -19,7 +27,11 @@ export function NewGroup() {
 
         <Input placeholder="Nome da turma" />
 
-        <Button title="Criar" style={{ marginTop: 20 }} />
+        <Button
+          title="Criar"
+          style={{ marginTop: 20 }}
+          onPress={handleNewGroup}
+        />
       </Content>
     </Container>
   );
