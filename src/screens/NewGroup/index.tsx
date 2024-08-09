486c1b4288425c91c4209d8bceee1944976b91b2
João Vitor Pereira <joaovitorpessoal2@gmail.com>
2024-08-09 00:46:12 -0300

    tratamento de erros

--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -6,6 +6,8 @@ import { Input } from "@components/Input";
 import { useNavigation } from "@react-navigation/native";
 import { useState } from "react";
 import { groupCreate } from "@storage/groups/groupCreate";
+import { AppError } from "@utils/AppError";
+import { Alert } from "react-native";
 
 export function NewGroup() {
   const navigation = useNavigation();
@@ -13,11 +15,19 @@ export function NewGroup() {
 
   async function handleNewGroup() {
     try {
+      if (group.trim().length === 0) {
+        throw new AppError("É necessário informar um nome para o grupo.");
+      }
       await groupCreate(group);
       navigation.navigate("players", {
         group,
       });
     } catch (error) {
+      if (error instanceof AppError) {
+        Alert.alert("Novo grupo: ", error.message);
+        return;
+      }
+      Alert.alert("Novo grupo: ", "Não foi possível criar um novo grupo");
       console.log(error);
     }
   }
